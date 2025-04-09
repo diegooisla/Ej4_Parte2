c3f8bbf3ae66b7c113e7085a7cdf9517199ee484
Diego <ortizisla10@gmail.com>
2025-04-09 15:59:37 +0200

    Paso 3

---
+++ src/parte2/Main.java
@@ -0,0 +1,18 @@
+package parte2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int a = 5, b = 5;
+		
+		int suma;
+		suma = a + b;
+		
+		System.out.println("Suma");
+		
+		System.out.println("Creando repo local como administrador");
+
+	}
+
+}
